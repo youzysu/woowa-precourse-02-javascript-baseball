d9f9f9e7a415c2570be92032f0825012ff47a770
shackstack <kms1061@naver.com>
2022-11-02 23:11:58 +0900

    feat: 입력값 받기 기능

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const RANDOM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let receivedNumber;
+    MissionUtils.Console.readLine("숫자를 입력해주세요.", (number) => {
+      receivedNumber = number;
+      console.log(receivedNumber);
+    });
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
