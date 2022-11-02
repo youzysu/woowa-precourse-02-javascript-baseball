d1c8eb661bf864325c244c92d1a2da3b386732c2
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-03 03:32:55 +0900

    feat: 정답 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const NUMBER_DIGISTS = 3;
 
 class App {
+  answer;
+
   play() {}
+
   printGameStartText() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  createAnswer() {
+    const randomNumberList = [];
+    while (randomNumberList.length < NUMBER_DIGISTS) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNumberList.includes(randomNumber)) {
+        randomNumberList.push(randomNumber);
+      }
+    }
+    this.answer = Number(randomNumberList.join(""));
+    return this.answer;
   }
 }
 
 const app = new App();
-app.play();
+console.log(app.createAnswer());
 
 module.exports = App;
