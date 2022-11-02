09ada869823309fa1246ce82be4931a7b2e8de47
shackstack <kms1061@naver.com>
2022-11-02 22:55:11 +0900

    feat: 랜덤 숫자 발급

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const RANDOM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
 
 module.exports = App;
