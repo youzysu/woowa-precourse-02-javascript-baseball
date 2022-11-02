d8d8d36683d6e5c72ff655edf0d94ee2da7d1aef
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-03 02:50:15 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+  printGameStartText() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
