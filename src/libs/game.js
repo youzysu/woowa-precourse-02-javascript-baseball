547e96d0caeacdebf66f9ad4af7b9ba977f2437d
nlom0218 <nlom0218@naver.com>
2022-11-03 23:58:21 +0900

    feat: 플레이어의 답변이 올바른 형식이 아니라면 애플리케이션 강제 종료하기
    문을 사용해 예외를 발생시킨후 애플리케이션은 종료한다.

--- src/libs/game.js
+++ src/libs/game.js
@@ -58,6 +58,8 @@ const game = {
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (input) => {
         const isValidPlayerInput = validation.option(input);
+
+        if (!isValidPlayerInput) return game.exitWithException();
       }
     );
   },
