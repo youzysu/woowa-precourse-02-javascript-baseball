947ea81fb8ead0afd65c6074e1dd3a1006864d1d
shackstack <kms1061@naver.com>
2022-11-03 10:42:28 +0900

    feat: 상대방의 수와 입력받은 숫자의 일치여부 확인

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const numberCheck = (number) => {
-  if (number.split("").length != 3) {
+  if (number.length != 3) {
     throw "잘못된 값을 입력하셨습니다. 게임을 종료합니다.";
   }
 };
+
+const pointCounter = (receive, random) => {
+  let ballStrikeCount = [0, 0];
+  receive.forEach((item, index) => {
+    if (item == random[index]) {
+      ballStrikeCount[1] += 1;
+    } else if (random.includes(Number(item))) {
+      ballStrikeCount[0] += 1;
+    }
+    console.log(ballStrikeCount);
+  });
+};
+
 class App {
   play() {
     const RANDOM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-
+    console.log(RANDOM_NUMBER);
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
     MissionUtils.Console.readLine("숫자를 입력해주세요.", (number) => {
-      let receivedNumber = number;
+      let receivedNumber = number.split("");
+
       numberCheck(receivedNumber);
+      pointCounter(receivedNumber, RANDOM_NUMBER);
     });
   }
 }
