9cbe8ea22d6d5d6b94740b7899e38aec082577a6
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-04 20:46:18 +0900

    test: 플레이 입력값 유효성 검사 App에서 BaseballGame으로 수정

--- __tests__/CheckValdationTest.js
+++ __tests__/CheckValdationTest.js
@@ -1,18 +1,18 @@
-const App = require("../src/App");
-const app = new App();
+const BaseBallGame = require("../src/BaseballGame");
+const baseballGame = new BaseBallGame();
 
 describe("플레이 입력값 유효성 검사", () => {
   test("3자리 수 인지 검사", () => {
-    expect(app.checkNumberDigits("123")).toEqual(true);
-    expect(app.checkNumberDigits(123)).toEqual(true);
-    expect(app.checkNumberDigits("")).toEqual(false);
-    expect(app.checkNumberDigits(1234)).toEqual(false);
+    expect(baseballGame.checkNumberDigits("123")).toEqual(true);
+    expect(baseballGame.checkNumberDigits(123)).toEqual(true);
+    expect(baseballGame.checkNumberDigits("")).toEqual(false);
+    expect(baseballGame.checkNumberDigits(1234)).toEqual(false);
   });
   test("0이 포함되어 있는지 검사", () => {
-    expect(app.checkIncludesZero("0")).toEqual(true);
-    expect(app.checkIncludesZero(0)).toEqual(true);
-    expect(app.checkIncludesZero(120)).toEqual(true);
-    expect(app.checkIncludesZero("123")).toEqual(false);
-    expect(app.checkIncludesZero("777")).toEqual(false);
+    expect(baseballGame.checkIncludesZero("0")).toEqual(true);
+    expect(baseballGame.checkIncludesZero(0)).toEqual(true);
+    expect(baseballGame.checkIncludesZero(120)).toEqual(true);
+    expect(baseballGame.checkIncludesZero("123")).toEqual(false);
+    expect(baseballGame.checkIncludesZero("777")).toEqual(false);
   });
 });
