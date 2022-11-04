b7ed6b812057d06483bb316ce1ad67adb51f25dc
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-04 20:49:24 +0900

    feat: 사용자가 입력한 수와 정답을 비교하기

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class Game {
+  constructor() {
+    this.strikeCount = 0
+    this.ballCount = 0
+  }
+
   setAnswerNumber() {
     const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
-    return numbers.join('')
+    return numbers
+  }
+
+  counterStrikeBall(userInput) {
+    const userNumber = userInput.toString().split('').map(str => Number(str))
+    const answerNumber = this.setAnswerNumber()
+
+    for (let index = 0; index < userNumber.length; index++) {
+      if (userNumber[index] === answerNumber[index]) this.strikeCount++
+      else {
+        if (answerNumber.includes(userNumber[index])) this.ballCount++
+      }
+    }
+    
+    const hint = { strike: this.strikeCount, ball: this.ballCount }
+    return hint
   }
 }
