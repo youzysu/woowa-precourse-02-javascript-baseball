d93ec46588505170fd3d448d63e4d9b14f87aaff
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-04 20:47:40 +0900

    test: 플레이어 입력값 결과 App에서 Baseballgame으로 수정

--- __tests__/CompareNumbersTest.js
+++ __tests__/CompareNumbersTest.js
@@ -1,9 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const App = require("../src/App");
-const app = new App();
-
-//테스트 케이스 콘솔 받는 법으로 수정
-//compareNumbers(answer, playerInputValue);
+const BaseBallGame = require("../src/BaseballGame");
+const baseballGame = new BaseBallGame();
 
 const getLogSpy = () => {
   return jest.spyOn(MissionUtils.Console, "print");
@@ -11,32 +8,29 @@ const getLogSpy = () => {
 
 describe("정답과 플레이어 입력값을 비교", () => {
   test("3개의 숫자를 모두 맞칠 경우", () => {
-    //given
     const message = "3스트라이크";
     const logSpy = getLogSpy();
 
-    //when
-    app.compareNumbers(713, 713);
+    baseballGame.compareNumbers(713, 713);
 
-    //then
     expect(logSpy).toHaveBeenCalledWith(message);
   });
 
   test("하나도 없는 경우", () => {
     const logSpy = getLogSpy();
-    app.compareNumbers(713, 245);
+    baseballGame.compareNumbers(713, 245);
     expect(logSpy).toHaveBeenCalledWith("낫싱");
   });
 
   test("볼과 스트라이크", () => {
     const logSpy = getLogSpy();
-    app.compareNumbers(713, 145);
+    baseballGame.compareNumbers(713, 145);
     expect(logSpy).toHaveBeenCalledWith("1볼");
-    app.compareNumbers(713, 671);
+    baseballGame.compareNumbers(713, 671);
     expect(logSpy).toHaveBeenCalledWith("2볼");
-    app.compareNumbers(713, 123);
+    baseballGame.compareNumbers(713, 123);
     expect(logSpy).toHaveBeenCalledWith("1볼 1스트라이크");
-    app.compareNumbers(713, 216);
+    baseballGame.compareNumbers(713, 216);
     expect(logSpy).toHaveBeenCalledWith("1스트라이크");
   });
 });
