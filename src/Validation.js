1234de493904707c61c058aaaa4eada84cad78c4
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-06 18:17:01 +0900

    fix: fixed input type

--- src/Validation.js
+++ src/Validation.js
@@ -1,28 +1,28 @@
 class Validation {
   isValidInput(userInput) {
-    this.typeValidity(userInput)
-    this.lengthValidity(userInput)
-    this.isDuplicate(userInput)
-    return true
+    this.typeValidity(userInput);
+    this.lengthValidity(userInput);
+    this.isDuplicate(userInput);
   }
 
   typeValidity(userInput) {
-    if (!Number.isInteger(userInput)) {
-      throw new Error('숫자만 입력해주세요.')
+    if (!Number.isInteger(Number(userInput))) {
+      throw new Error("숫자만 입력해주세요.");
     }
   }
 
   lengthValidity(userInput) {
-    if (userInput.toString().length !== 3) {
-      throw new Error('3자리 숫자를 입력해주세요.')
+    if (userInput.length !== 3) {
+      throw new Error("3자리 숫자를 입력해주세요.");
     }
   }
 
   isDuplicate(userInput) {
-    const StrNumber = userInput.toString()
-    const removedSame = new Set(StrNumber)
-    if (StrNumber.length !== removedSame.size) {
-      throw new Error('중복된 숫자가 있습니다.')
+    const removedSame = new Set(userInput);
+    if (userInput.length !== removedSame.size) {
+      throw new Error("중복된 숫자가 있습니다.");
     }
   }
 }
+
+module.exports = new Validation();
