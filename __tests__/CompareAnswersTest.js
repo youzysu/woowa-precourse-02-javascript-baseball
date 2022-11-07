8122f497188df884ba42c4d0d02e4ad3dc0eb468
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-07 18:15:20 +0900

    test: 정답 비교 테스트 상수 분리 한 것 적용

--- __tests__/CompareAnswersTest.js
+++ __tests__/CompareAnswersTest.js
@@ -1,5 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const BaseBallGame = require("../src/BaseballGame");
+const {
+  STRIKE,
+  NOTHING,
+  GAME_END_SENTENCE,
+  BALL,
+} = require("../src/constants");
 const baseballGame = new BaseBallGame();
 
 const getLogSpy = () => {
@@ -8,10 +14,7 @@ const getLogSpy = () => {
 
 describe("정답과 플레이어 입력값을 비교", () => {
   test("3개의 숫자를 모두 맞칠 경우", () => {
-    const messages = [
-      "3스트라이크",
-      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-    ];
+    const messages = [`3${STRIKE}`, GAME_END_SENTENCE];
     const logSpy = getLogSpy();
 
     baseballGame.compareAnswers(713, 713);
@@ -24,7 +27,7 @@ describe("정답과 플레이어 입력값을 비교", () => {
   test("하나도 없는 경우", () => {
     const logSpy = getLogSpy();
     baseballGame.compareAnswers(713, 245);
-    expect(logSpy).toHaveBeenCalledWith("낫싱");
+    expect(logSpy).toHaveBeenCalledWith(NOTHING);
   });
 
   const logCalledTimes = 3;
@@ -33,18 +36,18 @@ describe("정답과 플레이어 입력값을 비교", () => {
     const logSpy = getLogSpy();
 
     baseballGame.compareAnswers(713, 145);
-    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 1, "1볼");
+    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 1, `1${BALL}`);
 
     baseballGame.compareAnswers(713, 671);
-    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 2, "2볼");
+    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 2, `2${BALL}`);
 
     baseballGame.compareAnswers(713, 123);
     expect(logSpy).toHaveBeenNthCalledWith(
       logCalledTimes + 3,
-      "1볼 1스트라이크"
+      `1${BALL} 1${STRIKE}`
     );
 
     baseballGame.compareAnswers(713, 216);
-    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 4, "1스트라이크");
+    expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 4, `1${STRIKE}`);
   });
 });
