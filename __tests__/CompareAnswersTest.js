7fa84589e5359ee75050c98cba693739102b46d8
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-07 18:13:12 +0900

    test: 정답 비교 네이밍 수정

--- __tests__/CompareAnswersTest.js
+++ __tests__/CompareAnswersTest.js
@@ -14,7 +14,7 @@ describe("정답과 플레이어 입력값을 비교", () => {
     ];
     const logSpy = getLogSpy();
 
-    baseballGame.compareNumbers(713, 713);
+    baseballGame.compareAnswers(713, 713);
 
     messages.forEach((message) => {
       expect(logSpy).toHaveBeenCalledWith(message);
@@ -23,7 +23,7 @@ describe("정답과 플레이어 입력값을 비교", () => {
 
   test("하나도 없는 경우", () => {
     const logSpy = getLogSpy();
-    baseballGame.compareNumbers(713, 245);
+    baseballGame.compareAnswers(713, 245);
     expect(logSpy).toHaveBeenCalledWith("낫싱");
   });
 
@@ -32,19 +32,19 @@ describe("정답과 플레이어 입력값을 비교", () => {
   test("볼과 스트라이크", () => {
     const logSpy = getLogSpy();
 
-    baseballGame.compareNumbers(713, 145);
+    baseballGame.compareAnswers(713, 145);
     expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 1, "1볼");
 
-    baseballGame.compareNumbers(713, 671);
+    baseballGame.compareAnswers(713, 671);
     expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 2, "2볼");
 
-    baseballGame.compareNumbers(713, 123);
+    baseballGame.compareAnswers(713, 123);
     expect(logSpy).toHaveBeenNthCalledWith(
       logCalledTimes + 3,
       "1볼 1스트라이크"
     );
 
-    baseballGame.compareNumbers(713, 216);
+    baseballGame.compareAnswers(713, 216);
     expect(logSpy).toHaveBeenNthCalledWith(logCalledTimes + 4, "1스트라이크");
   });
 });
