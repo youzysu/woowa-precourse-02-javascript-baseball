533dfd6ffe48b4dcce1c628d3e854df86f327c90
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-25 22:12:38 +0900

    feat: deepFreeze 추가

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,20 @@
+const { deepFreeze } = require("./utils");
+
 const THREE_DIGISTS = 3;
 
-const COMMAND = Object.freeze({
+const COMMAND = {
   RESTART: 1,
   END: 2,
-});
+};
 
-const GAME_SENTENCE = Object.freeze({
+const GAME_SENTENCE = {
   START: "숫자 야구 게임을 시작합니다.",
   RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
   END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   PLAYER_INPUT: "숫자를 입력해주세요 : ",
-});
+};
 
-const COUNT = Object.freeze({
+const COUNT = {
   STRIKE: (count) => (count ? `${count}스트라이크` : ""),
   BALL: (count) => (count ? `${count}볼` : ""),
   NOTHING: "낫싱",
@@ -21,7 +23,10 @@ const COUNT = Object.freeze({
     ball: 0,
     strike: 0,
   },
-});
+};
+
+const exportsObject = { COMMAND, GAME_SENTENCE, COUNT };
+deepFreeze(exportsObject);
 
 module.exports = {
   COMMAND,
---
+++ src/utils.js
@@ -0,0 +1,17 @@
+const deepFreeze = (object) => {
+  const propNames = Object.getOwnPropertyNames(object);
+
+  for (const name of propNames) {
+    const value = object[name];
+
+    if (value && typeof value === "object") {
+      deepFreeze(value);
+    }
+  }
+
+  return Object.freeze(object);
+};
+
+module.exports = {
+  deepFreeze,
+};
