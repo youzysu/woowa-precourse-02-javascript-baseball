217ca162c072275b4a793f70d263ad6f1b052d90
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-25 17:54:47 +0900

    chore: eslint, prettier 설정
    Airbnb 자바스크립트 스타일 가이드 기준 설정

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
