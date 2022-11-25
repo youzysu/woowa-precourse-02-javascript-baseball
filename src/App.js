3d1f21da4350ece811cb1d72dd3ab402326f7cbb
유지수 Jisoo Yoo <youzysu@gmail.com>
2022-11-25 14:31:21 +0900

    Revert "merge"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-const Game = require('../src/Game');
-const { MESSAGE } = require('../src/Materials');
-const { Console } = require('@woowacourse/mission-utils');
-
 class App {
-  play() {
-    Console.print(MESSAGE.START);
-    Game.start();
-  }
+  play() {}
 }
 
 module.exports = App;
