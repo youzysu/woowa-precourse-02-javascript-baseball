75dca44858994bc5cab329204b0a07de6910d092
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-26 23:18:03 +0900

    feat: 사용자의 입력값 유효성 검증하기

---
+++ src/Model/Validator.js
@@ -0,0 +1,38 @@
+const { ERROR, OPTION } = require('../Util/Constants');
+
+const Validator = {
+  validateNumber(userInput) {
+    Validator.validateValue(userInput);
+    Validator.validateLength(userInput);
+    Validator.validateDuplicate(userInput);
+  },
+
+  validateValue(userInput) {
+    userInput.forEach((number) => {
+      if (!new RegExp('^[1-9]+$').test(number)) {
+        throw new Error(ERROR.VALUE);
+      }
+    });
+  },
+
+  validateLength(userInput) {
+    if (userInput.length !== 3) {
+      throw new Error(ERROR.LENGTH);
+    }
+  },
+
+  validateDuplicate(userInput) {
+    const removedSame = new Set(userInput);
+    if (userInput.length !== removedSame.size) {
+      throw new Error(ERROR.DUPLICATE);
+    }
+  },
+
+  validateOption(userInput) {
+    if (userInput !== OPTION.RESTART && userInput !== OPTION.END) {
+      throw new Error(ERROR.OPTION);
+    }
+  },
+};
+
+module.exports = Validator;
