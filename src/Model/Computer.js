08a04db45a361254358e75f77a7402e57df8de06
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-26 21:28:01 +0900

    feat: 사용자가 입력한 숫자와 컴퓨터가 만든 정답 비교

--- src/Model/Computer.js
+++ src/Model/Computer.js
@@ -9,7 +9,20 @@ class Computer {
       const number = Random.pickNumberInRange(1, 9);
       if (!correctNumbers.includes(number)) correctNumbers.push(number);
     }
+
     this.#correctNumbers = correctNumbers;
+  }
+
+  compareNumber(userNumber) {
+    const result = { strike: 0, ball: 0 };
+
+    for (let index = 0; index < userNumber.length; index++) {
+      if (userNumber[index] === this.#correctNumbers[index]) result.strike += 1;
+      else if (this.#correctNumbers.includes(userNumber[index])) result.ball += 1;
+    }
+
+    return result;
   }
 }
 
+module.exports = Computer;
